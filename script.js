da619d756791c3256b2f3af870211a421bcbf041
Raxxyy <161466309+RajChintawar@users.noreply.github.com>
2025-06-12 00:55:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+// Typing effect using Typed.js
+const typed = new Typed('#typed', {
+  strings: [
+    "Hi, I'm Raj Chintawar.",
+    "A Computer Science Student.",
+    "A Tech Enthusiast.",
+    "A Web Developer.",
+    "A Lifelong Learner."
+  ],
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 1500,
+  loop: true
+});
